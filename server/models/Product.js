cec642a3cd77ccac1ea9ce2870dbf91acb1c8ce8
N Z <nelectrik@gmail.com>
2023-02-18 12:07:20 +1030

    Details page work

--- server/models/Product.js
+++ server/models/Product.js
@@ -3,10 +3,6 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-  productID: {
-    type: Number,
-    required: true,
-  },
   name: {
     type: String,
     required: true,
