3d3e8788973fad948bf814f15ecb77bbd3ed659a
Billy Cheng <chengpitchi@yahoo.com>
2023-02-20 20:57:06 +1030

    add address and phone

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-console.log(process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI, {
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jewellery-e-commerce', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
