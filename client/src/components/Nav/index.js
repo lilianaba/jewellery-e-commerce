a41c184e5a9a886de27965785e1a00ec4b98a1c6
N Z <nelectrik@gmail.com>
2023-02-26 01:31:47 +1030

    Last changes on Readme file, added video and screenshot, fixed index.css

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -17,6 +17,9 @@ function Nav() {
               Logout
             </a>
           </li>
+          <li className="mx-1">
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       );
     } else {
